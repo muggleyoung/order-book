99db875df381947ebb687710e57aef5058536b66
Mei Yang <mei.yang.3@volvocars.com>
2024-10-26 22:11:02 +0200

    scaffold for a running application

---
+++ src/main/java/com/investment/entities/Order.java
@@ -0,0 +1,9 @@
+//@Entity
+//@Table(name = "order")
+//public class Order {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private UUID id;
+//
+//    private String ticker;
+//}
